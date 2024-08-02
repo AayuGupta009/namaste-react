3fbfd567cacd5dc60a15066be978e3dec4244d72
Aayush Gupta <aayush.gupta@appventurez.com>
2024-08-02 18:09:23 +0530

    igniting our app using parcel

--- App.js
+++ App.js
@@ -1,38 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const firstHeading = React.createElement(
   // React helper function used to create HTML elements. In the end , React.createElement (Your Code) : Object ==> HTML Element (Browser) : Understands
   "h1", // html element
   { id: "heading" }, // attributes
-  "I am the first heading!!!" // props or children
+  "I am the first heading!!!", // props or children
 );
 
 const secondHeading = React.createElement(
   "h2", // html element
   { id: "heading" }, // attributes
-  "I am the second heading!!!" // props or children
+  "I am the second heading!!!", // props or children
 );
 
 const firstChild = React.createElement(
   "div", // html element
   { id: "child1" }, // attributes
-  [firstHeading, secondHeading] // props or children
+  [firstHeading, secondHeading], // props or children
 );
 
 const secondChild = React.createElement(
   "div", // html element
   { id: "child2" }, // attributes
-  [firstHeading, secondHeading] // props or children
+  [firstHeading, secondHeading], // props or children
 );
 
 const thirdChild = React.createElement(
   "div", // html element
   { id: "child3" }, // attributes
-  [firstHeading, secondHeading] // props or children
+  [firstHeading, secondHeading], // props or children
 );
 
 const parent = React.createElement(
   "div", // html element
   { id: "parent" }, // attributes
-  [firstChild, secondChild, thirdChild] // props or children
+  [firstChild, secondChild, thirdChild], // props or children
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Namaste React
 
 This Repo contains assignments from Namaste React from Akshay Saini.
+
+# Parcel
+
+- Generates Build
+- Local Dev Server
+- HMR => Hot Module Replacement
+- File Watching Algo - written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compression
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Good Error Handling
+- Diagnostic
+- HTTPs
+- Tree Shaking - removes unused code.
